a0d7822c6700cda8b7f2cd964fe1beeb0f78d415
dloyolar <dieegolr@gmail.com>
2022-08-12 22:23:40 -0400

    feat: add register screen

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,10 +1,115 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
+import {WhiteLogo} from '../components/WhiteLogo';
+import {loginStyles} from '../theme/loginTheme';
+import {useForm} from '../hooks/useForm';
+import {StackScreenProps} from '@react-navigation/stack';
 
-export const RegisterScreen = () => {
+interface Props extends StackScreenProps<any, any> {}
+
+export const RegisterScreen = ({navigation}: Props) => {
+  const {email, name, password, onChange} = useForm({
+    email: '',
+    name: '',
+    password: '',
+  });
+
+  const onRegister = () => {
+    console.log({email, name, password});
+    Keyboard.dismiss();
+  };
+
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1, backgroundColor: '#5856D6'}}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <View style={loginStyles.formContainer}>
+          <WhiteLogo />
+
+          <Text style={loginStyles.title}>Registro</Text>
+
+          <Text style={loginStyles.label}>Nombre:</Text>
+          <TextInput
+            placeholder="Ingresa tu nombre"
+            placeholderTextColor="rgba(255,255,255,0.4)"
+            keyboardType="email-address"
+            underlineColorAndroid="white"
+            style={[
+              loginStyles.inputField,
+              Platform.OS === 'ios' && loginStyles.inputFieldIOS,
+            ]}
+            selectionColor="white"
+            onChangeText={value => onChange(value, 'email')}
+            value={email}
+            autoCapitalize="words"
+            onSubmitEditing={onRegister}
+            autoCorrect={false}
+          />
+
+          <Text style={loginStyles.label}>Email:</Text>
+          <TextInput
+            placeholder="Ingresa tu correo"
+            placeholderTextColor="rgba(255,255,255,0.4)"
+            keyboardType="email-address"
+            underlineColorAndroid="white"
+            style={[
+              loginStyles.inputField,
+              Platform.OS === 'ios' && loginStyles.inputFieldIOS,
+            ]}
+            selectionColor="white"
+            onChangeText={value => onChange(value, 'email')}
+            value={email}
+            autoCapitalize="none"
+            onSubmitEditing={onRegister}
+            autoCorrect={false}
+          />
+
+          <Text style={loginStyles.label}>Contraseña:</Text>
+          <TextInput
+            placeholder="**********"
+            placeholderTextColor="rgba(255,255,255,0.4)"
+            secureTextEntry
+            keyboardType="email-address"
+            underlineColorAndroid="white"
+            style={[
+              loginStyles.inputField,
+              Platform.OS === 'ios' && loginStyles.inputFieldIOS,
+            ]}
+            selectionColor="white"
+            onChangeText={value => onChange(value, 'password')}
+            onSubmitEditing={onRegister}
+            value={password}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+
+          <View style={loginStyles.buttomContainer}>
+            <TouchableOpacity
+              activeOpacity={0.8}
+              style={loginStyles.button}
+              onPress={onRegister}>
+              <Text style={loginStyles.buttonText}>Crear cuenta</Text>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => navigation.replace('LoginScreen')}
+            style={loginStyles.buttomReturn}>
+            <Text style={loginStyles.buttonText}>Ya tengo una cuenta</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </>
   );
 };
