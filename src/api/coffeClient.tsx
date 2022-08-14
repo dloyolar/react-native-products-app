76f93c980f57a68c8fd58c8ecfb699cb0d4f98a3
dloyolar <dieegolr@gmail.com>
2022-08-14 11:20:36 -0400

    feat: add interceptor for add token to api headers

--- src/api/coffeClient.tsx
+++ src/api/coffeClient.tsx
@@ -1,9 +1,18 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const baseURL = 'https://coffe-rn-node.herokuapp.com/api';
 
 const coffeClient = axios.create({
   baseURL,
+});
+
+coffeClient.interceptors.request.use(async config => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    config.headers!['x-token'] = token;
+  }
+  return config;
 });
 
 export default coffeClient;
