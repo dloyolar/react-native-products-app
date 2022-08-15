5277fae621682deb77c4fdf2539bbc2a99a2be35
dloyolar <dieegolr@gmail.com>
2022-08-15 15:35:55 -0400

    feat: add ui and data in ProductsScreen

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,10 +1,27 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {StackScreenProps} from '@react-navigation/stack';
+import {ProductsStackParams} from '../navigator/ProductsNavigator';
 
-export const ProductScreen = () => {
+interface Props
+  extends StackScreenProps<ProductsStackParams, 'ProductScreen'> {}
+
+export const ProductScreen = ({route, navigation}: Props) => {
+  const {id, name = ''} = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: name || 'Nuevo Producto',
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <View>
-      <Text>ProductScreen</Text>
+      <Text>
+        {id} - {name}
+      </Text>
     </View>
   );
 };
--- src/screens/ProductsScreen.tsx
+++ src/screens/ProductsScreen.tsx
@@ -1,10 +1,64 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useContext, useEffect} from 'react';
+import {View, Text, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
+import {ProductsContext} from '../context/ProductsContext';
+import {StackScreenProps} from '@react-navigation/stack';
+import {ProductsStackParams} from '../navigator/ProductsNavigator';
 
-export const ProductsScreen = () => {
+interface Props
+  extends StackScreenProps<ProductsStackParams, 'ProductsScreen'> {}
+
+export const ProductsScreen = ({navigation}: Props) => {
+  const {products} = useContext(ProductsContext);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={{marginRight: 10}}
+          onPress={() => navigation.navigate('ProductScreen', {})}>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+      ),
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <View>
-      <Text>ProductsScreen</Text>
+    <View style={styles.mainContainer}>
+      <FlatList
+        data={products}
+        keyExtractor={p => p._id}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() =>
+              navigation.navigate('ProductScreen', {
+                id: item._id,
+                name: item.nombre,
+              })
+            }>
+            <Text style={styles.productName}>{item.nombre}</Text>
+          </TouchableOpacity>
+        )}
+        ItemSeparatorComponent={() => <View style={styles.itemSeparator} />}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    marginHorizontal: 10,
+  },
+  productName: {
+    fontSize: 20,
+  },
+  itemSeparator: {
+    borderBottomWidth: 2,
+    marginVertical: 5,
+    borderBottomColor: 'rgba(0,0,0,0.1)',
+  },
+});
