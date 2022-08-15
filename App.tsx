4198678d41fa150c2434f0e77ea763f196dd6f54
dloyolar <dieegolr@gmail.com>
2022-08-15 13:30:56 -0400

    wip: ProductsContext

--- App.tsx
+++ App.tsx
@@ -4,12 +4,15 @@ import {NavigationContainer} from '@react-navigation/native';
 
 import {Navigator} from './src/navigator/Navigator';
 import {AuthProvider} from './src/context/AuthContext';
+import {ProductsProvider} from './src/context/ProductsContext';
 
 const App = () => {
   return (
     <NavigationContainer>
       <AuthProvider>
-        <Navigator />
+        <ProductsProvider>
+          <Navigator />
+        </ProductsProvider>
       </AuthProvider>
     </NavigationContainer>
   );
--- src/interfaces/appInterfaces.tsx
+++ src/interfaces/appInterfaces.tsx
@@ -25,3 +25,23 @@ export interface User {
   uid: string;
   img?: string;
 }
+
+// Products
+export interface ProductsResponse {
+  total: number;
+  productos: Producto[];
+}
+
+export interface Producto {
+  precio: number;
+  _id: string;
+  nombre: string;
+  categoria: Categoria;
+  usuario: Categoria;
+  img?: string;
+}
+
+export interface Categoria {
+  _id: string;
+  nombre: string;
+}
